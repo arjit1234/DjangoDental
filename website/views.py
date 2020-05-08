bed65c24044d80b50f11b688aa0256bbcc78acf3
arjit1234 <lit.arjitbiswall@gmail.com>
2020-05-08 12:34:24 +0530

    first_Commit

---
+++ website/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+# Create your views here.
+
+def home(request):
+
+	return render(request,'home.html',{})
+
+def contact(request):
+	if request.method=="POST":
+		#do stuff
+		message_name=request.POST['message-name']
+		message_email=request.POST['message-email']
+		message=request.POST['message']
+
+		#Send email
+
+		send_mail(
+			message_name,#subject
+			message,#message
+			message_email,#to email
+			['lit.arjitbiswall@gmail.com'],
+			fail_silently=False,
+
+			)
+		return render(request,'contact.html',{'message_name':message_name})
+	else:
+		#
+		return render(request,'contact.html',{})
